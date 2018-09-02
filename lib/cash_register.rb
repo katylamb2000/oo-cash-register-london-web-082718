71350bc0b86a0a7c40a87c8168f924dbd43d8e1a
katy@karstaway.com <katy@karstaway.com>
2018-09-02 16:44:47 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,8 +14,8 @@ def total
 end
 
 def add_item (title, price, quantity=1)
-  @transactions = []
-  @transactions << [title, (price * quantity = 1)]
+  # @transactions = []
+  # @transactions << [title, (price * quantity = 1)]
   @total += (price * quantity)
   
   quantity.times do
