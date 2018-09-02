be6f865e5411fe7083102c30be46d4dac88a0d0b
katy@karstaway.com <katy@karstaway.com>
2018-09-02 15:57:50 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,7 @@ end
 
 def apply_discount 
  if discount > 0
-   "After the discount, the total comes to #{total}."
+   "After the discount, the total comes to $#{total}."
   else
  "There is no discount to apply."
 end
