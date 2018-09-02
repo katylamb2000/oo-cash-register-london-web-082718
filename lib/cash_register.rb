db6df07dd9982655b77f53abd0dc3c952826f149
katy@karstaway.com <katy@karstaway.com>
2018-09-02 15:31:24 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+class CashRegister
+  
+  attr_accessor :total
+  
+  
+def initialize (total = 0)
+ @total = total
+  end
 
+
+def total (total, price)
+ @@total << this.price
+ 
+  end
+
+
+def apply_discount 
+ 
+end
+
+def add_item
+end
+
+end
