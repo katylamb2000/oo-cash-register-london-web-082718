0dba335c512c5a7b4778e71f93f1b535b12ef717
katy@karstaway.com <katy@karstaway.com>
2018-09-02 16:06:57 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,8 +18,8 @@ def add_item (title, price, quantity = 1)
 end
 
 def apply_discount 
-  @discount = 5
-  @total /= @discount
+  @discount = 0.8
+  @total *= @discount
  if discount > 0
    "After the discount, the total comes to $#{total}."
   else
